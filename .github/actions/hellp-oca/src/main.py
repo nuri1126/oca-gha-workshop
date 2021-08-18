39f1ffbaada3ee530adaa7ae9271ccb3ef5a80ea
nuri1126 <nuri1126@users.noreply.github.com>
2021-08-18 23:00:50 +0900

    Update hello-oca

--- .github/actions/hellp-oca/src/main.py
+++ .github/actions/hellp-oca/src/main.py
@@ -4,5 +4,5 @@ import os
 getEnvLeader = os.environ.get("INPUT_LEADER")
 getEnvMember = os.environ.get("INPUT_MEMBER")
 
-print("Hello " + getEnvLeader)
-print("Hello " + getEnvMember)
+print("Hello " + getEnvMember1)
+print("Hello " + getEnvMember2)
